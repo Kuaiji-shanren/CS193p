a9659cf070950d0aafb843e5c6d906ca89111242
Kuocangshan <nolightyears@gmail.com>
2024-03-27 15:50:27 +0100

    Initial Commit

---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by Orchidea on 2024/3/27.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
